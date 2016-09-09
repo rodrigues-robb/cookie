bd1eca62b45796fd40dedfd5d3df3d5e9f8d6d29
bpm1993 <bpm1993@icloud.com>
2016-09-09 11:59:05 -0300

    Versão sem referrer

--- cookie.js
+++ cookie.js
@@ -2,26 +2,34 @@ var get_previous_value = function(origin, cookie_name){
     var current_page    = document.URL;
     var referrer        = document.referrer;
     var current_cookie  = get_cookie(cookie_name);
- 
+	
     if(current_cookie){
-        var value   = current_cookie.split('|')[0];
-        var page    = current_cookie.split('|')[1];
-        if(page == referrer){
-            document.cookie = cookie_name + '=' + origin + '|' + current_page + ';path=/';
-            return value;
-        }
+        var _value   	= current_cookie.split('|')[0];
+        var page    	= current_cookie.split('|')[1];
+        if(page){
+			set_referrer_value(cookie_name, origin, current_page);
+			console.log('>>>>>>>>>>>>>>>>  ' + decodeURI( _value));
+            return decodeURI( _value);
+        } else {
+			set_referrer_value(cookie_name, origin, current_page);
+		}
        
     } else {
-        document.cookie = cookie_name + '=' + origin + '|' + current_page + ';path=/';
+        set_referrer_value(cookie_name, origin, current_page);
+		console.log('>>>>>>>>>>>>>>>>> Criado');
     }
 }
  
- 
+var set_referrer_value = function(cookie_name, origin, current_page){
+	if(typeof cookie_name === "string"){
+		document.cookie = cookie_name + '=' + encodeURI(origin) + '|' + current_page + ';domain=.' + document.domain + ';path=/';
+	}
+} 
  
 var get_cookie = function(cname){
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') {
             c = c.substring(1);
@@ -32,6 +40,5 @@ var get_cookie = function(cname){
     }
     return "";
 }
- 
- 
+
 get_previous_value(document.title, 'p_title');
